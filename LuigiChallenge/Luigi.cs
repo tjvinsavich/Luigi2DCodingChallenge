748f28fdccc6ba65be2b66a30a3006ff616d9bed
Whit Stroup <email@example.com>
2021-05-14 13:13:02 -0500

    added comments

--- LuigiChallenge/Luigi.cs
+++ LuigiChallenge/Luigi.cs
@@ -7,6 +7,28 @@ namespace LuigiChallenge
         {
         }
 
+
+//Guide Luigi home by following the trail of "0"s from left to right and top to bottom in the 2D matrix.
+
+//Give him directions with two commands:
+
+//"Down" or "Right";
+
+//Example:
+//input:
+
+//        [4,0,4]
+//        [5,0,0]
+//        [6,9,0]
+//        [8,3,0]
+
+//        output:
+
+//String "Down Right Down Down"
+
+//(Note: In all the tests Luigi will have clear path.So there will only be a "0" below or to the right of each other.No isolated "0"s or "0"s moving back left.)
+
+
         public string GoHome2D(int[][] map)
         {
             throw new NotImplementedException();
